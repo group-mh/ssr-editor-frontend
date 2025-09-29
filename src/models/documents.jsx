7650cdab39611950222b09f82f8866185953de5d
mats-jonis <majt24@student.bth.se>
2025-09-29 10:51:00 +0200

    Update config and document model for Cypress tests

--- src/models/documents.jsx
+++ src/models/documents.jsx
@@ -1,107 +1,103 @@
-/* const docModel = {
+const docModel = {
     baseUrl: window.location.href.includes("localhost")
-        ? "http://localhost:1337"
-        : "",
+        ? "https://jsramverk-editor-hahi24-byewf7bndbf9ehhf.swedencentral-01.azurewebsites.net"
+        : "https://jsramverk-editor-hahi24-byewf7bndbf9ehhf.swedencentral-01.azurewebsites.net",
     baseName: window.location.href.includes("localhost")
         ? "/"
-        : "/", */
+        : "/",
 
-    const docModel = {
-        baseUrl: "http://localhost:1337",
-        baseName: "/",
+        getAllDocs: async function getAllDocs() {
 
-    getAllDocs: async function getAllDocs() {
+            try {        
+                const response = await fetch(`${docModel.baseUrl}/docs`, {
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    method: "GET",
+                });
 
-        try {        
-            const response = await fetch(`${docModel.baseUrl}/docs`, {
-                headers: {
-                    "content-type": "application/json",
-                },
-                method: "GET",
-            });
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
 
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
+                const result = await response.json();
+                return result.data;
+            } catch (error) {
+                console.error("getAllDocs error:", error.message);
+                return [];
+            }        
+        },
+
+        createDoc: async function createDoc(newDoc) {
+            try {
+                const response = await fetch(`${docModel.baseUrl}/docs`, {
+                    body: JSON.stringify(newDoc),
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    method: "POST",
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                console.log("createDoc response result:", result);
+                return result;
+            } catch (error) {
+                console.error("createDoc error:", error.message);
+                return null;
             }
+        },
 
-            const result = await response.json();
-            return result.data;
-        } catch (error) {
-            console.error("getAllDocs error:", error.message);
-            return [];
-        }        
-    },
-
-    createDoc: async function createDoc(newDoc) {
-        try {
-            const response = await fetch(`${docModel.baseUrl}/docs`, {
-                body: JSON.stringify(newDoc),
-                headers: {
-                    "content-type": "application/json",
-                },
-                method: "POST",
-            });
-
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-
-            const result = await response.json();
-            console.log("createDoc response result:", result);
-            return result;
-        } catch (error) {
-            console.error("createDoc error:", error.message);
-            return null;
-        }
-    },
-
-    updateDoc: async function updateDoc(updateDoc) {
-        try {
-            const { _id, ...updateData } = updateDoc;
-        const response = await fetch(
-            `${docModel.baseUrl}/docs/${_id}`,
-            {
-                body: JSON.stringify(updateData),
-                headers: {
-                    "content-type": "application/json",
-                },
-                method: "PUT",
-            });
-        
-            if(!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
+        updateDoc: async function updateDoc(updateDoc) {
+            try {
+                const { _id, ...updateData } = updateDoc;
+            const response = await fetch(
+                `${docModel.baseUrl}/docs/${_id}`,
+                {
+                    body: JSON.stringify(updateData),
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    method: "PUT",
+                });
+            
+                if(!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                
+                const result = await response.json();
+                return result;
+            } catch (error) {
+                console.error("updateDoc error:", error.message);
+                return null;
             }
             
-            const result = await response.json();
-            return result;
-        } catch (error) {
-            console.error("updateDoc error:", error.message);
-            return null;
-        }
-        
-    },
+        },
 
-    deleteDoc: async function deleteDoc(id) {
-        try {
-            const response = await fetch(
-            `${docModel.baseUrl}/docs/${id}`,
-            {
-                method: "DELETE",
-            });
+        deleteDoc: async function deleteDoc(id) {
+            try {
+                const response = await fetch(
+                `${docModel.baseUrl}/docs/${id}`,
+                {
+                    method: "DELETE",
+                });
 
-            if(!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
+                if(!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                return result;
+            } catch (error) {
+                console.error("deleteDoc error:", error.message);
+                return null;
             }
-
-            const result = await response.json();
-            return result;
-        } catch (error) {
-            console.error("deleteDoc error:", error.message);
-            return null;
         }
-    }
-};
+    };
 
 
 
-export default docModel;
+    export default docModel;
