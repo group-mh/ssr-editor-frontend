8c0f8bbbd3f68d6c5048239cd08d94fb85c59a3d
hihassan1998 <hassanihussain1998@gmail.com>
2025-09-29 21:11:47 +0200

    update cypress configurations with longer timeputs

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,        
     },
   },
 );
