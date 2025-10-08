e76313ae3702f72efcafe18ca58bab2b95ddce6f
mats-jonis <majt24@student.bth.se>
2025-10-08 14:40:10 +0200

    Add login button to header

--- src/components/incl/Header.jsx
+++ src/components/incl/Header.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "../../style/Header.css";
 
-function Header() {
+function Header({ user, onLogout }) {
   const navigate = useNavigate();
 
   return (
@@ -12,6 +12,11 @@ function Header() {
       <nav className="nav-buttons">
         <button className="nav-btn" onClick={() => navigate("/")}>Home</button>
         <button className="nav-btn" onClick={() => navigate("/create")}>Create document</button>
+        {user?.token ? (
+        <button className="nav-btn" onClick={onLogout}>Logout</button>
+        ) : (
+        <button className="nav-btn" onClick={() => navigate("/login")}>Login</button>
+        )}
       </nav>
     </header>
   );
